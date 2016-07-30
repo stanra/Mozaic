7060a51933652fc059961ea1e59488e0f967e466
stanra <stan.radoux@gmail.com>
2016-07-30 15:37:57 +0800

    small important corrections
    not tested but tried. seems to work

--- arrays.py
+++ arrays.py
@@ -1,7 +1,7 @@
 import datetime
 import numpy as np
 from first_tests import _get_lowest_fraction
-
+import math
 
 def cut(picture, param_x, param_y, mode, fixed_size):
     if mode == 'number':
@@ -16,8 +16,8 @@ def cut(picture, param_x, param_y, mode, fixed_size):
 
     elif mode == 'size':
         tile_size = param_x, param_y
-        num_tile = tuple(p_s//t_s for p_s, t_s in zip(picture.shape, tile_size))
-        fraction = tuple( (0,num) for num in num_tile) # maybe should num-1 but it doesn't actually change anything
+        num_tile = tuple(int(math.ceil(p_s/t_s)) for p_s, t_s in zip(picture.shape, tile_size))
+        fraction = tuple((0,num) for num in num_tile) # maybe should num-1 but it doesn't actually change anything. actually better like that. The last picture 'wants' to be same size.
     else:
         raise ValueError('Valid modes : "number" or "size"')
 
@@ -50,22 +50,23 @@ def cut(picture, param_x, param_y, mode, fixed_size):
     return mozaic, info
 
 
-
 def stitch(mozaic, info, top_left, down_right, fixed_size):
 
     # V1 : consider all tile have same size --> it is faster.
     if fixed_size:
-        top_left_tile = (tile // size for tile, size in zip(top_left, info['tile_size']))
-        down_right_tile = (tile // size for tile, size in zip(down_right, info['tile_size']))
+        top_left_tile = tuple(tile // size for tile, size in zip(top_left, info['tile_size']))
+        down_right_tile = tuple(tile // size for tile, size in zip(down_right, info['tile_size']))
     else:
         top_left_tile = _get_tile_including(top_left, info)
         down_right_tile = _get_tile_including(down_right, info)
 
+    print(top_left_tile)
+    print(down_right_tile)
     lines = []
     i = 0
-    for tile_x in range(top_left_tile[0], down_right_tile[0]):
+    for tile_x in range(top_left_tile[0], down_right_tile[0]+1):
         cur_line = mozaic[(tile_x,top_left_tile[1])]
-        for tile_y in range(top_left_tile[1]+1, down_right_tile[1]):
+        for tile_y in range(top_left_tile[1], down_right_tile[1]+1):
             cur_line = np.concatenate((cur_line, mozaic[(tile_x,tile_y)]),axis=1)
         lines.append(cur_line)
 
@@ -81,10 +82,10 @@ def _get_tile_including(point, info):
     # but the time saved is not going to be a lot and the code will get more annoying.
 
 
-    tile_size_avg = tuple(pic_size / num_tile for pic_size, num_tile in zip(info['original_size'], info['tile_size']))
+    tile_size_avg = tuple(pic_size / num_tile for pic_size, num_tile in zip(info['original_size'], info['num_tiles']))
     expected_real_pos = tuple( point_pos / tile_size for point_pos, tile_size in zip(point, tile_size_avg))
 
-    result = (None,None)
+    result = [None, None]
     for i in (0,1): # We treat x and y separately
         p,q = info['fraction'][i]
         tile_size = info['tile_size'][i]
@@ -93,7 +94,7 @@ def _get_tile_including(point, info):
             num_finished_cycle = index_guessed_tile // q
             p_prime = index_guessed_tile % q # p_prime is the position in the current cycle
 
-            pos_guessed_tile = num_finished_cycle * tile_size_avg[i] +\
+            pos_guessed_tile = num_finished_cycle * q * tile_size_avg[i] +\
                                     (tile_size+1) * min(p,p_prime) + tile_size * max(p_prime-p,0)
             error = point[i] - pos_guessed_tile
 
@@ -109,9 +110,10 @@ def _get_tile_including(point, info):
                     result[i] = index_guessed_tile
                     break
 
-    return result
+    return tuple(result)
 
-
-
-
-
+if __name__ == '__main__':
+    pic = np.reshape(['({},{})'.format(i, j) for i in range(79) for j in range(100)], (79, 100))
+    moz, info = cut(pic, 60, 7, 'number', False)
+    res = stitch(moz, info, (0, 0), (65, 19), False)
+    print(res)
